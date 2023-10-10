9c6bdefc50935a433096851a2f388c081f48b945
Jeffrey Chang <jeffchang192@gmail.com>
2023-10-10 14:16:42 -0700

    fix compile issue

--- app/src/main/java/me/jeffreychang/weatherapp/model/onecall/Weather.kt
+++ app/src/main/java/me/jeffreychang/weatherapp/model/onecall/Weather.kt
@@ -1,10 +1,8 @@
 package me.jeffreychang.weatherapp.model.onecall
 
 
-import androidx.compose.runtime.Composable
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import me.jeffreychang.weatherapp.Sun
 
 @Serializable
 data class Weather(
@@ -16,12 +14,4 @@ data class Weather(
     val id: Int,
     @SerialName("main")
     val main: String
-) {
-    @Composable
-    fun toResource(): @Composable () -> Unit {
-        return when (id) {
-            800 -> ({ Sun() })
-            else -> ({ Sun() })
-        }
-    }
-}
+)
